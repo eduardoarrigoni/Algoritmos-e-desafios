ff4b54db401e6c7524d235e7fd0ee9c5d517374a
eduardoarrigoni <eduardo.souza.arrigoni@gmail.com>
2025-09-17 10:42:58 -0300

    solução dos poblema

---
+++ Sales By Match/salesByMatch.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function sockMerchant(n, ar) {
+    
+    let contador = 0;
+    ar.sort();
+    let i = 0;
+    while(i < n - 1){
+        
+        if(ar[i] == ar[i+1]){
+            contador++;
+            i +=2;
+        }else{
+            i++;
+        }
+    }
+    return contador;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
