802e633806c81442f88a17fe4542e1600620214a
eduardoarrigoni <eduardo.souza.arrigoni@gmail.com>
2025-09-25 18:01:29 -0300

    tratando erros e ajustando a saida

--- contagemPalavras/contagem.js
+++ contagemPalavras/contagem.js
@@ -4,14 +4,42 @@ const caminhoArquivo = process.argv;
 const local = caminhoArquivo[2];
 
 fs.readFile(local, "utf-8", (erro, texto) => {
-    divisaoParagrafos(texto);
+    try{
+        if(erro) throw erro;
+        divisaoParagrafos(texto);
+
+    }catch(erro){
+
+        if(erro.code === 'ENOENT') console.log('Caminho errado');
+        else console.log('Outro problema');
+    }
     
 })
 function divisaoParagrafos(texto){
 
     const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.flatMap(paragrafo => {
+        if(!paragrafo) return [];
+        return contagemPalavras(refinaTexto(paragrafo));
+
+    })
+    console.log(contagem);
+}
+function refinaTexto(texto){
+    return texto.replace(/[.,\/#?!$%\^&\*;:{}=\r\-_`~()]/g, '');;
 }
 function contagemPalavras(texto){
 
+    const objetosDuplicados = {}
+    const palavrasSeparadas = texto.split(' ');
+
+    palavrasSeparadas.forEach(element => {
+        
+        if (element.length >= 3) {
+
+            objetosDuplicados[element] = (objetosDuplicados[element] || 0) + 1
+        }
+    });
+    return objetosDuplicados;
     
 }
